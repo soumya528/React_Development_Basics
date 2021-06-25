ddd7fd70ce5aeb834b3591af23f3845ab482cbd3
soumya528 <62896878+soumya528@users.noreply.github.com>
2021-06-25 17:49:33 +0530

    Ul And LI Also Bit Of CSS

--- src/App.js
+++ src/App.js
@@ -10,25 +10,38 @@ function Header(props) {
 }
 function Main(props) {
   return (
-    <section>
-      <h3> Choose your {props.Toppings}</h3>
+    <section style={{ textAlign: "left" }}>
+      <h3> Toppings</h3>
+      <ul>
+        {props.Topping.map((top) => (
+          <li>{top}</li>
+        ))}
+      </ul>
     </section>
   );
 }
 function Footer(props) {
   return (
     <section>
-      <h3> Choose your {props.Deserts}</h3>
+      <h3 style={{ textAlign: "left" }}> Deserts </h3>
+      <ul>
+        {props.Desert.map((SweetTooth) => (
+          <li style={{ textAlign: "left" }}>{SweetTooth}</li>
+        ))}
+      </ul>
+      <p style={{ textAlign: "Centre" }}> Copyright ={props.year}</p>
     </section>
   );
 }
+const Topping = ["Peperoni", "Sausage", "Chello Kebab", "Butter Chicken"];
+const Desert = ["Red Velvet", "Cheese Cake", "Truffles", "Firni"];
+
 function App() {
   return (
     <div className="App">
       <Header Food="Pizza" />
-      <Main Toppings="Peperoni" />
-      <Footer Deserts="Puddings" />
-      <h2>Choose your magical Toppings happen!</h2>
+      <Main Topping={Topping} />
+      <Footer Desert={Desert} year={new Date().getFullYear()} />
     </div>
   );
 }
